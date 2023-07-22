334c7ce3d3d2cd950b880caef23460ba14fb9332
ImNotMenduina <mynameis_menduina@outlook.com>
2023-07-22 15:07:56 -0300

    feature: add method isEmpty to interface

--- stackQueue/src/model/queue/Queue.java
+++ stackQueue/src/model/queue/Queue.java
@@ -1,7 +1,7 @@
-package stackQueue;
+package model.queue;
 
 public interface Queue {
 	public abstract void add(int n);
-	public abstract void remove(int n);
+	public abstract void remove();
 	public abstract boolean isEmpty();
 }
